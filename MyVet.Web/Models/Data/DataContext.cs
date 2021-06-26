fc4a6143b85bbc1f94da7536108706d29b47e111
Cristian Camilo Marulanda <cristian@somosmakers.co>
2021-06-25 20:36:53 -0500

    Add common and webproject

---
+++ MyVet.Web/Models/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Models.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
